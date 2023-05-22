3b4f469d0ce05865fd01273c4db0563c4028f141
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-22 10:45:30 +0500

    vazifa-1 done

---
+++ vazifa-1.js
@@ -0,0 +1,10 @@
+let teacher = {
+    name: "Michael",
+    surname: "Jackson",
+    type: "Music",
+    phoneNumber: +99899654656,
+    address: "Parkwood street, 23",
+    salary: 30000,
+};
+
+console.log(teacher.name);
